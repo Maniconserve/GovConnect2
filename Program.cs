f24836605fef43c7034c8359fda2883407bea497
manideep vemula <manideep.vemula@cognine.com>
2024-12-02 19:05:12 +0530

    Refactor

--- Program.cs
+++ Program.cs
@@ -52,6 +52,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStaticFiles();
 app.UseStatusCodePagesWithReExecute("/Citizen/HandleError", "?statusCode={0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
